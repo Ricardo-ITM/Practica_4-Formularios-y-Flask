7526ecd318f86497a2b0a9d3b7588dd6b129fcf8
Jose Ricardo Telles Castro <programa.dream@gmail.com>
2025-03-25 22:25:12 -0700

    Asignacion de las rutas, redireccionamiento dinamico, vinculando hoja de estilos usando url_for()

--- practica_4/app/app.py
+++ practica_4/app/app.py
@@ -3,3 +3,19 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
 	return render_template("public/index.html")
+
+@app.route("/sobremi")
+def sobremi():
+	return render_template("public/sobre.html")
+
+@app.route("/contacto")
+def contacto():
+	return render_template("public/contacto.html")
+
+@app.route("/blog")
+def blog():
+	return render_template("public/blog.html")
+
+@app.route("/portafolio")
+def portafolio():
+	return render_template("public/portafolio.html")
